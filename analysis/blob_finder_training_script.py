01c8c9f56630e3a266c81aaa675884e8161a4fe7
Jae Hyeon Lee <jaekor91@gmail.com>
2018-04-08 21:25:25 -0400

    Made changes to blob_finder training script

--- analysis/blob_finder_training_script.py
+++ analysis/blob_finder_training_script.py
@@ -9,21 +9,21 @@ from sklearn.utils import shuffle
 # Loading and shuffling the train data
 fname = "./blob_finder_training_data/blob_finder_train_data.npz"
 samples_train = np.load(fname)
-Nsample_train = samples_train["image"].shape[0]
-data_train = samples_train['image'].reshape((Nsample_train, 32, 32, 1))
+Nsample_train = samples_train["sample"].shape[0]
+data_train = samples_train['sample'].reshape((Nsample_train, 32, 32, 2))
 targets_train = samples_train['label'] # True if not blank
 data_train, targets_train = shuffle(data_train, targets_train) #, random_state=0)
 
 # Loading and shuffling the test data
 fname = "./blob_finder_training_data/blob_finder_test_data.npz"
 samples_test = np.load(fname)
-Nsample_test = samples_test["image"].shape[0]
-data_test = samples_test['image'].reshape((Nsample_test, 32, 32, 1))
+Nsample_test = samples_test["sample"].shape[0]
+data_test = samples_test['sample'].reshape((Nsample_test, 32, 32, 2))
 targets_test = samples_test['label'] # True if not blank
 data_test, targets_test = shuffle(data_test, targets_test) #, random_state=0)
 
 # Splitting the data
-N_train = min(512 * 1000, Nsample_train)
+N_train = min(512 * 10, Nsample_train)
 train_data = data_train[:N_train, :, :]
 train_targets = targets_train[:N_train]
 
@@ -71,16 +71,16 @@ plt.close()
 
 
 
-# # ---- Making the model and training
-# model = ResnetBuilder.build(input_shape=(1, 32, 32), num_outputs=1, block_fn='basic_block', repetitions=[3, 4, 6, 3])
+# ---- Making the model and training
+model = ResnetBuilder.build(input_shape=(2, 32, 32), num_outputs=1, block_fn='basic_block', repetitions=[3, 4, 6, 3])
 
-# import keras
-# callbacks_list = [keras.callbacks.ModelCheckpoint(filepath = 'classification_model.h5', monitor = "val_loss", save_best_only=True)]
+import keras
+callbacks_list = [keras.callbacks.ModelCheckpoint(filepath = 'classification_model.h5', monitor = "val_loss", save_best_only=True)]
 
 
-# model.compile(optimizer=optimizers.RMSprop(lr=0.00001), loss=losses.binary_crossentropy, metrics=['binary_crossentropy', 'accuracy'])
+model.compile(optimizer=optimizers.RMSprop(lr=0.00001), loss=losses.binary_crossentropy, metrics=['binary_crossentropy', 'accuracy'])
 
-# history = model.fit(train_data, train_targets, epochs=2, batch_size=256, validation_data=(val_data, val_targets))
+history = model.fit(train_data, train_targets, epochs=2, batch_size=256, validation_data=(val_data, val_targets))
 
-# # model.save('classification_model.h5')
+# model.save('classification_model.h5')
 
