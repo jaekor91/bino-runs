ef6e06724d1fefdcae1405578568dd755d473877
Jae Hyeon Lee <jaekor91@gmail.com>
2018-04-08 22:41:53 -0400

    Training example generation updated to incorporate error information

--- analysis/generate_blob_finder_train_data.py
+++ analysis/generate_blob_finder_train_data.py
@@ -1,21 +1,33 @@
 from utils import *
 
-im_arr_blobs = np.copy(np.load("./blob_finder_training_data/real-blobs.npz")["image"])
-im_arr_blanks = np.copy(np.load("./blob_finder_training_data/blanks-filtered.npz")["image"])
+print("/---- Train set")
+im_arr_blobs = np.copy(np.load("./blob_finder_training_data/real-blobs.npz")["image"]) * 1e16
+err_arr_blobs = np.copy(np.load("./blob_finder_training_data/real-blobs.npz")["err"]) * 1e16
+im_arr_blanks = np.copy(np.load("./blob_finder_training_data/blanks-filtered.npz")["image"]) * 1e16
+err_arr_blanks = np.copy(np.load("./blob_finder_training_data/blanks-filtered.npz")["err"]) * 1e16
 
-# Basic parameters
-# - Total flux could range 2,500 to 25,000
-# - Diagonal 2D covariance 16 = r^2 of varying from 0.1 to 0.9 randomly
+# --- Basic parameters
+# - SN threshold set at 25
+# - Multiply everything by 1e16 factor
+
+# --- Important considerations
+# - Input both noise and values.
+# - Visual inspection of positives
+# - Zero out the original zero points
+
+# --- During testing
+# - If more than 80% is blank, then pass.
 
 
-def blob_im_generator(nrows=32, ncols=32, double=False, fmin=0):
+def blob_im_generator(nrows=32, ncols=32, double=False, fdensity=0):
     """
     Generate an emission line peak blob.
     """
-#     fmin = max(20000, fmin)
-    fmax = fmin * 1.01
-    FWHM_min = 4
+    FWHM_min = 3
     FWHM_max = 6
+    FWHM = (FWHM_max - FWHM_min) * np.random.random() + FWHM_min # FWHM selection    
+    fmin = 10 * FWHM**2 * fdensity * (5 + 5 * np.random.random())
+    fmax = fmin * 1.01
     rho_min = -0.6
     rho_max = 0.6
     num_comps_min = 5
@@ -29,29 +41,24 @@ def blob_im_generator(nrows=32, ncols=32, double=False, fmin=0):
     im = np.zeros((nrows, ncols))
     x = nrows//2 + (np.random.random() - 0.5) * 3
     y = ncols//2 + (np.random.random() - 0.5) * 3
+    
+    f = (fmax - fmin) * np.random.random() + fmin # Random flux selection
+    rho = (rho_max - rho_min) * np.random.random() + rho_min # Covarince selection
+    num_comps = np.random.randint(num_comps_min, num_comps_max, 1)[0]
+    scatter = np.random.random() * scatter_max    
     if not double:
-        f = (fmax - fmin) * np.random.random() + fmin # Random flux selection
-        rho = (rho_max - rho_min) * np.random.random() + rho_min # Covarince selection
-        FWHM = (FWHM_max - FWHM_min) * np.random.random() + FWHM_min # FWHM selection
-        num_comps = np.random.randint(num_comps_min, num_comps_max, 1)[0]
-        scatter = np.random.random() * scatter_max
         peak = f * generalized_gauss_PSF(nrows, ncols, x,  y, FWHM=FWHM, rho=rho, scatter=scatter, num_comps=num_comps) # Double peak
         im += peak # Add peak
     else:
         sep_peaks = np.random.random() * (sep_peaks_max - sep_peaks_min) + sep_peaks_min # Separation in peaks in pixels
-        f = (fmax - fmin) * np.random.random() + fmin # Random flux selection
-        rho = (rho_max - rho_min) * np.random.random() + rho_min # Covarince selection
-        FWHM = (FWHM_max - FWHM_min) * np.random.random() + FWHM_min # FWHM selection
-        num_comps = np.random.randint(num_comps_min, num_comps_max, 1)[0]
-        scatter = np.random.random() * scatter_max
         peak1 = f * generalized_gauss_PSF(nrows, ncols, x,  y-sep_peaks/2., FWHM=FWHM, rho=rho, scatter=scatter, num_comps=num_comps) # Double peak
         peak2 = f * generalized_gauss_PSF(nrows, ncols, x,  y+sep_peaks/2., FWHM=FWHM, rho=rho, scatter=scatter, num_comps=num_comps) # Double peak    
         im += (peak1 + peak2) # Add peak    
     
-    return poisson_realization(im)
+    return poisson_realization(im * 10) / 10 # Use arbitrary counts-to-flux conversion.
 
 Nsample = 512 * 1000
-im_sim_training = np.zeros((Nsample, 32, 32, 1))
+im_sim_training = np.zeros((Nsample, 32, 32, 2))
 label_training = np.zeros(Nsample, dtype=bool)
 
 idx = 0 
@@ -60,7 +67,8 @@ while idx < Nsample:
     if (idx % 5000) == 0:
         print(idx)
     idx_blank = np.random.choice(num_blanks) # Choose a random blank
-    im = np.copy(im_arr_blanks[idx_blank]) # Take the blank
+    im = np.copy(im_arr_blanks[idx_blank]) # Take the blank and err
+    err = np.copy(err_arr_blanks[idx_blank])
     
     im_strip = im[6:25, :].flatten()
     im_strip_low = np.percentile(im_strip, 80) # Noise level
@@ -75,57 +83,93 @@ while idx < Nsample:
         else:
             B = np.std(im_strip)
 
-        fmin = B * 400 * (10 + 1 * np.random.random()) # Assume the blob spans 400 pixels and the "S2N > 10".
         r = np.random.random()
     #     r = 0
         if (r < 0.5): # Add the blank to the training data
             pass
         elif (r > 0.5) and (r < 0.55):
-            im_blob = blob_im_generator(double=False, fmin=fmin)
+            im_blob = blob_im_generator(double=False, fdensity = B)
             im += im_blob
             label_training[idx] = True
         else:
-            im_blob = blob_im_generator(double=True, fmin=fmin/2.)
+            im_blob = blob_im_generator(double=True, fdensity = B)
             im += im_blob
             label_training[idx] = True
             
         # Add poisson noise to the image
         # Generate poisson noise image, add the to the blank and subtract
-        im_poisson = poisson_realization(np.ones((32, 32)) * B) - B    
+        im_poisson = (poisson_realization(np.ones((32, 32)) * B * 10) - B) / 10
+        ibool = im == 0
         im += im_poisson
-        im[:5, :] = 0
-        im[26:, :] = 0
+        im[ibool] = 0 # Restore zero positions
         
         # Save the image
-        im_sim_training[idx] = im.reshape((1, 32, 32, 1))
+        im_sim_training[idx, :, :, 0] = im
+        im_sim_training[idx, :, :, 1] = err
         idx += 1
     
-# ---- View a sample of images.
+np.savez("./blob_finder_training_data/blob_finderx_train_data.npz", sample=im_sim_training, label=label_training)
+
+# ---- View only the positives
 plt.close()
 fig, ax_list = plt.subplots(9, 9, figsize=(10, 10))
 
-i_start = 0
-i_end = i_start + 81
-for i in range(i_start, i_end):
-    idx_row = (i-i_start) // 9
-    idx_col = (i-i_start) % 9
-    ax_list[idx_row, idx_col].imshow(im_sim_training[i, :, :, 0], cmap="gray", interpolation="none") # , vmin=vmin, vmax=vmax)
-#     title_str = "%4d" % (label_training[i])
-    title_str = label_training[i]
-    ax_list[idx_row, idx_col].set_title(title_str, fontsize=5)
-    ax_list[idx_row, idx_col].axis("off")    
+num_panels = 10
+idx = 0
+for l in range(num_panels):
+    counter = 0
+    while counter < 81:
+        if label_training[idx]: 
+            idx_row = counter // 9
+            idx_col = counter % 9
+            ax_list[idx_row, idx_col].imshow(im_sim_training[idx, :, :, 0], cmap="gray", interpolation="none") # , vmin=vmin, vmax=vmax)
+        #     title_str = "%4d" % (label_training[i])
+            title_str = label_training[idx]
+            ax_list[idx_row, idx_col].set_title(title_str, fontsize=5)
+            ax_list[idx_row, idx_col].axis("off") 
+            counter += 1
+        idx +=1
 
-plt.savefig("blob_sim_training_examples.png", dpi=200, bbox_inches="tight")
+    plt.savefig("blob_sim_training_examples_%d.png" % l, dpi=200, bbox_inches="tight")
 # plt.show()
 plt.close()    
+
+plt.close()
+fig, ax_list = plt.subplots(9, 9, figsize=(10, 10))
+
+num_panels = 10
+idx = 0
+for l in range(num_panels):
+    counter = 0
+    while counter < 81:
+        if not label_training[idx]: 
+            idx_row = counter // 9
+            idx_col = counter % 9
+            ax_list[idx_row, idx_col].imshow(im_sim_training[idx, :, :, 0], cmap="gray", interpolation="none") # , vmin=vmin, vmax=vmax)
+        #     title_str = "%4d" % (label_training[i])
+            title_str = label_training[idx]
+            ax_list[idx_row, idx_col].set_title(title_str, fontsize=5)
+            ax_list[idx_row, idx_col].axis("off") 
+            counter += 1
+        idx +=1
+
+    plt.savefig("blanks_sim_training_examples_%d.png" % l, dpi=200, bbox_inches="tight")
+# plt.show()
+plt.close()    
+
+np.savez("./blob_finder_training_data/blob_finder_train_data.npz", sample=im_sim_training[:50000], label=label_training[:50000])
 
 print("Completed")
 
 
-np.savez("./blob_finder_training_data/blob_finder_train_data.npz", image=im_sim_training, label=label_training)
 
-im_arr_blobs = np.load("./blob_finder_training_data/real-blobs.npz")["image"]
-im_arr_blanks = np.load("./blob_finder_training_data/blanks-filtered.npz")["image"]
+
+
+print("/---- Test set")
+im_arr_blobs = np.copy(np.load("./blob_finder_training_data/real-blobs.npz")["image"]) * 1e16
+err_arr_blobs = np.copy(np.load("./blob_finder_training_data/real-blobs.npz")["err"]) * 1e16
+im_arr_blanks = np.copy(np.load("./blob_finder_training_data/blanks-filtered.npz")["image"]) * 1e16
+err_arr_blanks = np.copy(np.load("./blob_finder_training_data/blanks-filtered.npz")["err"]) * 1e16
 
 
 N_blanks = im_arr_blanks.shape[0]
@@ -134,5 +178,17 @@ N_total = N_blanks + N_blobs
 label = np.zeros(N_total, dtype=bool)
 label[:N_blanks] = False # Blank is False
 label[N_blanks:] = True
-im_arr = np.vstack((im_arr_blanks, im_arr_blobs))
-np.savez("./blob_finder_training_data/blob_finder_test_data.npz", image=im_arr, label=label)
+im_arr = np.zeros((N_total, 32, 32, 2))
+
+# ---- Blanks first
+for i in range(N_blanks):
+    im_arr[i, :, :, 0] = im_arr_blanks[i]
+    im_arr[i, :, :, 1] = err_arr_blanks[i]    
+
+# ---- Blobs second
+for i in range(N_blobs):
+    im_arr[i + N_blanks, :, :, 0] = im_arr_blobs[i]
+    im_arr[i + N_blanks, :, :, 1] = err_arr_blobs[i]    
+
+np.savez("./blob_finder_training_data/blob_finder_test_data.npz", sample=im_arr, label=label)
+print("Completed")
