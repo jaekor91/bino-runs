71b7a37876591a5693e65634b12de99aeb3d0259
Jae Hyeon Lee <jaekor91@gmail.com>
2018-05-22 20:24:12 -0400

    Script to generate test data

---
+++ analysis_final/generate-test-data.py
@@ -0,0 +1,15 @@
+# ----- Test data
+blobs = np.load("./train_data/blobs_filtered2.npy")
+blanks = np.load("./train_data/blanks_filtered1.npy")
+
+blobs_SN = blobs[:, 0, :, :]/blobs[:, 1, :, :]
+blanks_SN = blanks[:, 0, :, :]/blanks[:, 1, :, :]
+
+N_blobs = blobs_SN.shape[0]
+N_blanks = blanks_SN.shape[0]
+SN = np.vstack((blobs_SN, blanks_SN))
+
+# --- Compute target
+target = np.zeros(N_blobs+N_blanks, dtype=bool)
+target[:N_blobs] = True
+np.savez("./test_data/test_SN.npz", SN=SN, target=target)
