9e345c94e493e0f6af80bf622ec0a2a4a3704aef
Jae Hyeon Lee <jaekor91@gmail.com>
2018-05-23 01:18:06 -0400

    Do not regenerate blank examples

--- analysis_final/generate-train-data.py
+++ analysis_final/generate-train-data.py
@@ -6,7 +6,7 @@ iblack_list_blanks = blanks["black_list"]
 blanks = blanks["data"]
 
 N_blanks = blanks.shape[0]
-Nsample = 128 * 20
+Nsample = 128 * 10
 SN_train = np.zeros((Nsample, 32, 32), dtype=float)
 target_train = np.zeros(Nsample, dtype=bool)
 
@@ -29,6 +29,7 @@ for i in range(Nsample):
         target_train[i] = True
         
 np.savez("./train_data/train_data.npz", SN=SN_train, target=target_train)
+# Finished generated train data.
 
 # ---- Plot five panels of blank/blob        
 N_panels = 5 
@@ -36,8 +37,7 @@ blobs_train = SN_train[target_train]
 for i in range(N_panels):
     plot_post_stamps(blobs_train[i*100:(i+1)*100], fname="./train_data/fig_sim_data/blobs-%d.png" % i)
     
-
-blanks_train = SN_train[~target_train]
-for i in range(N_panels):
-    plot_post_stamps(blanks_train[i*100:(i+1)*100], fname="./train_data/fig_sim_data/blanks-%d.png" % i)
+# blanks_train = SN_train[~target_train]
+# for i in range(N_panels):
+#     plot_post_stamps(blanks_train[i*100:(i+1)*100], fname="./train_data/fig_sim_data/blanks-%d.png" % i)
 
