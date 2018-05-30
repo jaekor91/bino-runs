fe46cca82fd241d78952cb35c1fb1f1ce9b24038
Jae Hyeon Lee <jaekor91@gmail.com>
2018-05-30 14:27:15 -0400

    Corrected the bit numbers corresponding to RF selection from 2017C Commissioning.

--- analysis_final/combine-result-catalogs.py
+++ analysis_final/combine-result-catalogs.py
@@ -45,7 +45,40 @@ for key in union_catalog.keys():
     
 
 # --- Fix 2017C-commissioning bits
-# It appears that there is no reason to.
+# Import RF data
+data_NGC = fits.open("../2017C-commissioning/List_RF_NGC.fits")[1].data
+data_SGC = fits.open("../2017C-commissioning/List_RF_SGC.fits")[1].data
+data = np.hstack([data_NGC, data_SGC])
+sel1 = data["sel_ELG_RF"]
+sel2 = data["sel_ELG_RF_Loose"]
+sel3 = data["sel_ELG_RF_g"]
+ra_RF = data["ra"]
+dec_RF = data["dec"]
+
+
+# Iterate through each bit and change the bit to what it should have been.
+RA = union_catalog["RA"]
+DEC = union_catalog["DEC"]
+BIT = union_catalog["BIT"]
+MASK_NUM = union_catalog["MASK_NUM"]
+
+counter = 0
+for i in range(RA.size):
+    # Fix the bit if
+    # - Mask number is 0, 1, or 2: Immediately set 256 or higher to zero.
+    # - Find the matching RA and use the entry in RF list to fix the bit
+    if (RA[i] > 0) and (MASK_NUM[i] in [0, 1, 2]) and bit_true(BIT[i], 2**8 + 2**9 + 2**10):
+        idx1, idx2 = crossmatch_cat1_to_cat2(np.array([RA[i]]), np.array([DEC[i]]), ra_RF, dec_RF, tol=1./(deg2arcsec+1e-12))
+        if (idx1.size > 0):
+            union_catalog["BIT"][i] = np.bitwise_and(2**1 + 2**2 + 2**3 + 2**4 + 2**5 + 2**6 + 2**7, BIT[i])
+            if sel1[idx2] == 1:
+                union_catalog["BIT"][i] += 2**8
+            if sel2[idx2] == 1:
+                union_catalog["BIT"][i] += 2**9
+            if sel3[idx2] == 1:
+                union_catalog["BIT"][i] += 2**10
+            counter += 1
+# --- Save
 np.save("union-catalog-results.npy", union_catalog)
 
 data = np.load("union-catalog-results.npy").item()
