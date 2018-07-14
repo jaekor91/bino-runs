155ddc51ac1c6cfee85e8d79dc8c2fa5151b355e
Jae Hyeon Lee <jaekor91@gmail.com>
2018-07-14 13:49:32 -0400

    Added lines to print tables without RF.

--- analysis_final/make-summary-tables.py
+++ analysis_final/make-summary-tables.py
@@ -52,7 +52,7 @@ for m in range(len(save_dirs)):
         os.mkdir(save_dir)
 
     # Concentrate on objects of interest
-    data = np.load("union-catalog-results.npy").item()
+    data = np.load("union-catalog-results-fluxed.npy").item()
     BIT = data["BIT"][ibool]
     RA = data["RA"][ibool]
     DEC = data["DEC"][ibool]
@@ -219,3 +219,81 @@ for m in range(len(save_dirs)):
     output.write(print_str2 + "\n")    
     output.close()
 
+
+
+
+
+    # --------------- Tally table, DENSE without RF and Latex readable
+    output = open(save_dir + "table-dense-noRF.txt", "w")
+    # output.write("Tally by mask-selection\n")
+    # Header string
+    header_str = "%-15s &"  % ("Mask Name")
+    # output.write("-" * 165 + "\n")
+    for sel in SELECTIONS[:4]:
+        header_str += ("%-16s &" % 	sel)
+    output.write(header_str + "\n")
+    # output.write("-" * 165 + "\n")
+
+    for i in range(0, 16, 1):
+        # Select by mask.
+        ibool = (MASK_NUM == i) 
+        if (ibool.sum() > 0):
+            # Iteration through the following bits
+            print_str = "%-15s &" % (MASK_NAMES[i])
+            for j, bit in enumerate(BIT_CODES[:4]):
+                iselect = (np.bitwise_and(2**bit, BIT) > 0) & ibool
+                Nsample = iselect.sum() # Number of samples in the selection
+                Nz = ((REDZ[iselect] > 0.6) & (REDZ[iselect] < 1.6)).sum() # Number with redshift
+                if Nsample == 0:
+                    rate = 0
+                else:
+                    rate = Nz/Nsample * 100            
+                print_str += ("%4d/%4d (%2.1f) &" % (Nz, Nsample, rate))
+                
+            output.write(print_str + "\n")
+        
+    # --- Total tally by selection
+    # Tally by redshift bin
+    print_str = "%-15s &" % ("z [0.6 1.6]")
+    print_str001 = "%-15s &" % ("z [0, 0.6]")
+    print_str002 = "%-15s &" % ("z [0.6, 1.1]")
+    print_str003 = "%-15s &" % ("z [1.1, 1.6]")    
+    # Tally by redshift bin - total expected number
+    print_str2 = "%-15s &" %   ("z [0.6 1.6]")
+    print_str004 = "%-15s &" % ("z [0, 0.6]")
+    print_str005 = "%-15s &" % ("z [0.6, 1.1]")
+    print_str006 = "%-15s &" % ("z [1.1, 1.6]")    
+
+    for j, bit in enumerate(BIT_CODES[:4]):
+        iselect = (np.bitwise_and(2**bit, BIT) > 0)
+        Nsample = iselect.sum()# Number of samples in the selection
+        Nz = ((REDZ[iselect] > 0.6) & (REDZ[iselect] < 1.6)).sum() # Number with redshift [0.6, 1.6]
+        redz = REDZ[iselect] # All the redshifts in the selection
+        Nz001 = ((redz < 0.6) & (redz > 0)).sum() # Number of samples with redshift < 0.6
+        Nz002 = ((redz > 0.6) & (redz < 1.1)).sum() # Number of samples with redshift < 0.6
+        Nz003 = ((redz > 1.1) & (redz < 1.6)).sum() # Number of samples with redshift < 0.6
+        Nsample_true = Nsample        
+        if Nsample == 0:
+            Nsample = 1
+        print_str += ("%4d/%4d (%2.1f) &" % (Nz, Nsample_true, Nz/Nsample * 100))
+        print_str001 += "%4d/%4d (%2.1f) &" % (Nz001, Nsample_true, Nz001/Nsample * 100)
+        print_str002 += "%4d/%4d (%2.1f) &" % (Nz002, Nsample_true, Nz002/Nsample * 100)
+        print_str003 += "%4d/%4d (%2.1f) &" % (Nz003, Nsample_true, Nz003/Nsample * 100)
+        print_str2 += ("%15d &") % (Nz/Nsample * Ndensity_nominal[j])        
+        print_str004 += ("%15d &") % (Nz001/Nsample * Ndensity_nominal[j])
+        print_str005 += ("%15d &") % (Nz002/Nsample * Ndensity_nominal[j])
+        print_str006 += ("%15d &") % (Nz003/Nsample * Ndensity_nominal[j])
+
+    # output.write("-" * 165 + "\n")
+    output.write("Expected efficiency\n")    
+    output.write(print_str001 + "\n")
+    output.write(print_str002 + "\n")
+    output.write(print_str003 + "\n") 
+    output.write(print_str + "\n")
+    # output.write("-" * 165 + "\n")
+    output.write("Expected z-density\n")        
+    output.write(print_str004 + "\n")
+    output.write(print_str005 + "\n")
+    output.write(print_str006 + "\n")        
+    output.write(print_str2 + "\n")        
+    output.close()
